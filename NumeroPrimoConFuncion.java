ec8a3fd3a7df43b68db21ac6444f7d340472b537
JavierIbarraC <jibarra2508@gmail.com>
2024-10-24 12:26:41 +0200

    Add files via upload

---
+++ NumeroPrimoConFuncion.java
@@ -0,0 +1,27 @@
+public class NumeroPrimoConFuncion {
+
+    public static void main(String[] args){
+
+        System.out.println("Introduzca un número entero positvio");
+        int n = Integrer.parseInt(System.console().readLine());
+
+        if(EsPrimo){
+            System.out.println ("El " + n + " es primo.");
+        }
+        else{
+            System.out.println ("El " + n + " no es primo.");
+        }
+    }
+
+    
+
+    public static boolean EsPrimo(in x){
+        for (int i = 2; i < x; i++){
+            if((x % i) == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
